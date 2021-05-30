d35621d08ae6bf7ec35f7f9de15020ff458200e2
LiandaDuanDal <ln387408@dal.ca>
2021-05-29 23:00:22 -0300

    assignment 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import { Media } from 'reactstrap';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
   CardTitle } from 'reactstrap';
+import DishDetail_info from './DishdetailComponent.js';
 
 class Menu extends Component {
 
     constructor(props) {
         super(props);
-
+//used to store some property of the component
         this.state = {
             selectedDish: null
-        }
+        };
+        console.log('Menu component constructed');
     }
 
     onDishSelect(dish) {
@@ -33,12 +35,35 @@ class Menu extends Component {
                 <div></div>
             );
     }
+    onDishSelect(dish) {
+      this.setState({ selectedDish: dish});
+  }
+// redercomment
+  renderDish_comment(dish) {
+      if (dish != null)
+          return(
+              <Card>
+                  {/* <CardImg top src={dish.image} alt={dish.name} /> */}
+                  <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                  </CardBody>
+              </Card>
+          );
+      else
+          return(
+              <div></div>
+          );
+  }
 
     render() {
+      // make the menucode short
         const menu = this.props.dishes.map((dish) => {
             return (
+              // render the menu
               <div  className="col-12 col-md-5 m-1">
                 <Card key={dish.id}
+                // define the click antion
                   onClick={() => this.onDishSelect(dish)}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
@@ -51,14 +76,21 @@ class Menu extends Component {
 
         return (
             <div className="container">
+
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
+                {/*  */}
+                {/* <div className="row">
                   <div  className="col-12 col-md-5 m-1">
                     {this.renderDish(this.state.selectedDish)}
                   </div>
-                </div>
+                </div> */}
+                {/* then render the detail version  */}
+
+              <DishDetail_info dish={this.state.selectedDish} />
+
+
             </div>
         );
     }
