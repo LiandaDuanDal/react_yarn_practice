7b834c65afdb716bfa2c6037bd4c67bd6bb56c05
LiandaDuanDal <ln387408@dal.ca>
2021-06-03 15:57:21 -0300

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,29 +1,134 @@
-import React, { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
+// import React, { Component } from "react";
+// import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
 
-class DishDetail extends Component{
-    constructor(props) {
-        super(props);
+// class DishDetail extends Component{
+//     constructor(props) {
+//         super(props);
 
-        console.log(props);
+//         console.log(props);
         
-        // stores iproperties of this component
-        // ==============================
-        this.state = {
-            // selectedDishDetail_info: this.props.dsdetail
-            selectedDishDetail:null
+//         // stores iproperties of this component
+//         // ==============================
+//          state = {
+//             // selectedDishDetail_info:  props.dsdetail
+//             selectedDishDetail:null
             
-        };
-        // ==============================
+//         };
+//         // ==============================
 
-        console.log("this.state---Dishdetial--->",this.state);
-        // undefined--->this.props.dsdetail
-        console.log("dstail---->",this.props.dsdetail);
+//         console.log(" state---Dishdetial--->", state);
+//         // undefined---> props.dsdetail
+//         console.log("dstail---->", props.dsdetail);
 
-    }
-    //渲染菜单本身（图片+纯标题）
-    renderDish(dish) {
+//     }
 
+//     componentDidMount(){
+//         console.log("Dishdetail componentDidMount");
+//     }
+//     componentDidUpdate(){
+//         console.log("Dishdetail componentDidupdate");
+//     }
+
+
+
+
+
+//     //渲染菜单本身（图片+纯标题）
+//      RenderDish(dish) {
+
+//         if (dish != null) {
+//             return (
+//                 <div className='col-12 col-md-5 m-1'>
+//                     <Card>
+//                         <CardImg width="100%" src={dish.image} alt={dish.name} />
+//                         <CardBody>
+//                             <CardTitle> {dish.name}</CardTitle>
+//                             <CardText> {dish.description} </CardText>
+//                         </CardBody>
+//                     </Card>
+//                 </div>   
+//             );
+//         }
+//         else {
+//             return (
+//                 <div></div>
+//             );
+//         }
+//     }
+// // 渲染评论
+//     RenderComments(comments){
+//         if (comments == null) {
+//             return (<div></div>)
+//         }
+//         // console.alert(comments.type());
+//         const cmnts = comments.map((comment) => {
+//             return (
+//                 <li key={comment.id} class ="text-left">
+//                     <p>{comment.comment}</p>
+//                     <p>-- {comment.author},
+//                     &nbsp;
+//                     {new Intl.DateTimeFormat('en-US', {
+//                         year: 'numeric',
+//                         month: 'long',
+//                         day: '2-digit'
+//                     }).format(new Date(comment.date))}
+//                     </p>
+//                 </li>
+//             )
+//         })
+//         return (
+//             <div className='col-12 col-md-5 m-1'>
+//                 <h4 class = "text-left"> Comments </h4>
+//                 <ul className='list-unstyled'>
+//                     {/* 此处是经过map循环返回的一系列comment */}
+//                     {cmnts}
+//                 </ul>
+
+//             </div>
+//         )
+//     }
+//     // Props are arguments passed into React components.
+//     // Props are passed to components via HTML attributes.
+//     // https://www.w3schools.com/react/react_state.asp
+//     render(){
+//         const dish =  props.dish;
+
+//         // ==============================
+//         console.log("DishdetailComponent render 被调用");
+//         console.log("dish------->",dish);
+//         // ==============================
+//         if (dish == null) {
+//             return (<div></div>);
+//         }
+//         // ==============================
+//         const dishItem =   RenderDish(dish);
+//         const dishComment =  RenderComments(dish.comments);
+//         // ==============================
+//         console.log("连续渲染一个菜的详细信息+评论");
+//         // ==============================
+//         return (
+//             // 连续渲染一个菜的详细信息+评论
+//             <div class = "container">
+//                 <div className='row'>
+//                     {dishItem}
+//                     {dishComment}
+//                 </div>
+//             </div>
+//         )
+//     }
+
+// }
+
+// export default DishDetail;
+
+
+// ================
+
+import React from 'react';
+import { Card, CardImg, CardText, CardBody,CardTitle } from 'reactstrap';
+
+
+    function  RenderDish({dish}) { 
         if (dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
@@ -42,10 +147,14 @@ class DishDetail extends Component{
                 <div></div>
             );
         }
+
     }
-// 渲染评论
-    renderComments(comments){
+
+    function RenderComments({comments}) {
+        console.log("render commnets");
+        console.log("comments--->",comments);
         if (comments == null) {
+            console.log("empty comment!!!");
             return (<div></div>)
         }
         // console.alert(comments.type());
@@ -64,6 +173,7 @@ class DishDetail extends Component{
                 </li>
             )
         })
+        console.log("return comment");
         return (
             <div className='col-12 col-md-5 m-1'>
                 <h4 class = "text-left"> Comments </h4>
@@ -74,12 +184,13 @@ class DishDetail extends Component{
 
             </div>
         )
+      
     }
-    // Props are arguments passed into React components.
-    // Props are passed to components via HTML attributes.
-    // https://www.w3schools.com/react/react_state.asp
-    render(){
-        const dish = this.props.dish;
+
+    const  DishDetail = (props) => {
+
+        const dish =  props.dish;
+        // console.log(dish);
 
         // ==============================
         console.log("DishdetailComponent render 被调用");
@@ -89,8 +200,8 @@ class DishDetail extends Component{
             return (<div></div>);
         }
         // ==============================
-        const dishItem = this.renderDish(dish);
-        const dishComment = this.renderComments(dish.comments);
+        const dishItem =   RenderDish(dish);
+        const dishComment =  RenderComments(dish.comments);
         // ==============================
         console.log("连续渲染一个菜的详细信息+评论");
         // ==============================
@@ -98,13 +209,16 @@ class DishDetail extends Component{
             // 连续渲染一个菜的详细信息+评论
             <div class = "container">
                 <div className='row'>
-                    {dishItem}
-                    {dishComment}
+                <RenderDish dish = {props.dish}/>
+                <RenderComments comments = {props.dish.comments}/>
+
+                    {/* {dishItem}
+                    {dishComment} */}
                 </div>
             </div>
         )
     }
-
-}
+      
+    
 
 export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,124 +1,160 @@
-import React, { Component } from 'react';
-// import { Media } from 'reactstrap';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-  CardTitle } from 'reactstrap';
+// import React, { Component } from 'react';
+// // import { Media } from 'reactstrap';
+// import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+//   CardTitle } from 'reactstrap';
 
-//导入dishinfo
-// import DishDetailo from './DishdetailComponent.js';
+// //导入dishinfo
+// // import DishDetailo from './DishdetailComponent.js';
 
-// 新建menu类继承自componeng
-class Menu extends Component {
+// // 新建menu类继承自componeng
+// class Menu extends Component {
 
-    constructor(props) {
-        super(props);
-//used to store some property of the component
-// javascript 快速定义对象的方式
-// 类对象state  属性 selecedDish 值为null
+//     constructor(props) {
+//         super(props);
+// //used to store some property of the component
+// // javascript 快速定义对象的方式
+// // 类对象state  属性 selecedDish 值为null
 
-// 此时这个state是固化在Menu内部的，有一个自定义的属性selectedDish: null
-// 通过this.setState({ selectedDish: dish});可以设置这个state（间接)
-        this.state = {
-            selectedDish: null
-        };
-        console.log('Menu component constructed');
+// // 此时这个state是固化在Menu内部的，有一个自定义的属性selectedDish: null
+// // 通过this.setState({ selectedDish: dish});可以设置这个state（间接)
+//         this.state = {
+//             selectedDish: null
+//         };
+//         console.log('Menu component constructed');
+//     }
+
+//     // onDishSelect(dish) {
+//     //     this.setState({ selectedDish: dish});
+//     // }
+// // 实现renderDush方法传入dish应该，一道菜
+//     // renderDish(dish) {
+//     //     if (dish != null)
+//     //         return(
+//     //             <Card>
+//     //                 <CardImg top src={dish.image} alt={dish.name} />
+//     //                 <CardBody>
+//     //                   <CardTitle>{dish.name}</CardTitle>
+//     //                   <CardText>{dish.description}</CardText>
+//     //                 </CardBody>
+//     //             </Card>
+//     //         );
+//     //     else
+//     //         return(
+//     //             <div></div>
+//     //         );
+//     // }
+
+
+//     // 
+
+
+//   // onDishSelect(dish) {
+//   //   // setState()是从componet继承过来的
+//   //   // 传进去的参数是{键:值}
+//   //   this.setState({ selectedDish: dish});
+//   // }
+
+// // redercomment渲染评论
+//   // renderDish_comment(dish) {
+//   //     if (dish != null)
+//   //         return(
+//   //             <Card>
+//   //                 {/* <CardImg top src={dish.image} alt={dish.name} /> */}
+//   //                 <CardBody>
+//   //                   <CardTitle>{dish.name}</CardTitle>
+//   //                   <CardText>{dish.description}</CardText>
+//   //                 </CardBody>
+//   //             </Card>
+//   //         );
+//   //     else
+//   //         return(
+//   //             <div></div>
+//   //         );
+//   // }
+
+//     render() {
+//       // make the menu code short
+//       // 指定每一个card的渲染方式
+//       // 并使用map执行遍历--单次遍历渲染一次所有元素
+//       // props也是component的属性
+
+//       // 当一个dish从app.js扔给Menu，用this.props来读取这个属性
+//       // 当声明这个menu继承自componet这个类的时候，它的底层就自动有了prop。
+//       // 这里面储存了所有传给他的参数。
+//       // 这个dishes是跟着Main里面的dishes走的
+//         const menu = this.props.dishes.map((dish) => {
+//           // 这种()=>的函数写法不用声明函数名，直接往括号里方参数
+//           // 用了一次就不用了
+//             return (
+//               // render the menu
+//               <div  className="col-12 col-md-5 m-1">
+//                 {/* 这里指定了一个card对应了这个dish的id */}
+//                 <Card key={dish.id}
+//                         onClick={() => this.props.onClick(dish.id)}>
+//                   <CardImg width="100%" src={dish.image} alt={dish.name} />
+//                   <CardImgOverlay>
+//                       <CardTitle>{dish.name}</CardTitle>
+//                   </CardImgOverlay>
+//                 </Card>
+//               </div>
+//             );
+//         });
+
+//         return (
+//           // 所有小的菜谱单元包含在container里面
+//             <div className="container">
+
+//                 <div className="row">
+//                   {/* 先渲染上述的基础菜单 */}
+//                     {menu}
+//                 </div>
+//                 {/* 这个MenuComponent里面的selectDish本来是null */}
+//                 {/* 但是一旦调用了onclick方法，这个selectDish就有了card对应的dish */}
+//                 {/* 于是DishDetailo就可以利用这个dish */}
+//                 {/* 点击动作发生，读取当前state对应的dish值 */}
+//               {/* <DishDetailo dish={this.state.selectedDish} /> */}
+//             </div>
+//         );
+//     }
+// }
+// // The state object is where you store property values that belongs to the component.
+
+// // When the state object changes, the component re-renders.
+// // export the compunnet from the file
+// export default Menu;
+
+import React from 'react';
+import { Card, CardImg, CardImgOverlay,CardTitle } from 'reactstrap';
+
+    function RenderMenuItem ({dish, onClick}) {
+        return (
+            <Card
+                onClick={() => onClick(dish.id)}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        );
     }
 
-    // onDishSelect(dish) {
-    //     this.setState({ selectedDish: dish});
-    // }
-// 实现renderDush方法传入dish应该，一道菜
-    // renderDish(dish) {
-    //     if (dish != null)
-    //         return(
-    //             <Card>
-    //                 <CardImg top src={dish.image} alt={dish.name} />
-    //                 <CardBody>
-    //                   <CardTitle>{dish.name}</CardTitle>
-    //                   <CardText>{dish.description}</CardText>
-    //                 </CardBody>
-    //             </Card>
-    //         );
-    //     else
-    //         return(
-    //             <div></div>
-    //         );
-    // }
+    const Menu = (props) => {
 
-
-    // 
-
-
-  // onDishSelect(dish) {
-  //   // setState()是从componet继承过来的
-  //   // 传进去的参数是{键:值}
-  //   this.setState({ selectedDish: dish});
-  // }
-
-// redercomment渲染评论
-  // renderDish_comment(dish) {
-  //     if (dish != null)
-  //         return(
-  //             <Card>
-  //                 {/* <CardImg top src={dish.image} alt={dish.name} /> */}
-  //                 <CardBody>
-  //                   <CardTitle>{dish.name}</CardTitle>
-  //                   <CardText>{dish.description}</CardText>
-  //                 </CardBody>
-  //             </Card>
-  //         );
-  //     else
-  //         return(
-  //             <div></div>
-  //         );
-  // }
-
-    render() {
-      // make the menu code short
-      // 指定每一个card的渲染方式
-      // 并使用map执行遍历--单次遍历渲染一次所有元素
-      // props也是component的属性
-
-      // 当一个dish从app.js扔给Menu，用this.props来读取这个属性
-      // 当声明这个menu继承自componet这个类的时候，它的底层就自动有了prop。
-      // 这里面储存了所有传给他的参数。
-      // 这个dishes是跟着Main里面的dishes走的
-        const menu = this.props.dishes.map((dish) => {
-          // 这种()=>的函数写法不用声明函数名，直接往括号里方参数
-          // 用了一次就不用了
+        const menu = props.dishes.map((dish) => {
             return (
-              // render the menu
-              <div  className="col-12 col-md-5 m-1">
-                {/* 这里指定了一个card对应了这个dish的id */}
-                <Card key={dish.id}
-                        onClick={() => this.props.onClick(dish.id)}>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
-              </div>
+                <div className="col-12 col-md-5 m-1"  key={dish.id}>
+                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                </div>
             );
         });
 
         return (
-          // 所有小的菜谱单元包含在container里面
             <div className="container">
-
                 <div className="row">
-                  {/* 先渲染上述的基础菜单 */}
                     {menu}
                 </div>
-                {/* 这个MenuComponent里面的selectDish本来是null */}
-                {/* 但是一旦调用了onclick方法，这个selectDish就有了card对应的dish */}
-                {/* 于是DishDetailo就可以利用这个dish */}
-                {/* 点击动作发生，读取当前state对应的dish值 */}
-              {/* <DishDetailo dish={this.state.selectedDish} /> */}
             </div>
         );
     }
-}
-// The state object is where you store property values that belongs to the component.
 
-// When the state object changes, the component re-renders.
-// export the compunnet from the file
 export default Menu;
