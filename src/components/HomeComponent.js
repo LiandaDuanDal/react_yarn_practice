b8ec0a36fbd5956f3ad413d51c04ee306ec8abbe
LiandaDuanDal <ln387408@dal.ca>
2021-06-04 17:36:03 -0300

    Single Page Applications Part 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle} from 'reactstrap';
+
+function RenderCard({item}) {
+
+    return(
+        // alt 属性指定图像不能正常显示（网速慢、图片链接错误）后显示的替换文本。
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+            <CardTitle>{item.name}</CardTitle>
+            {/* 检查是否存在，不存在用null代替 */}
+            {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+            <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+
+}
 
 function Home(props) {
     return(
-      <div className="container">
-        <h4>Home</h4>
-      </div>
+        <div className="container">
+            <div className="row align-items-start">
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.dish} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.promotion} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.leader} />
+                </div>
+            </div>
+        </div>
     );
 }
 
-export default Home; 
+export default Home;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -12,15 +12,23 @@ import Footer from './FooterComponent';
 // 
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+//
+import Contact from './ContactComponent';
+// 
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
-    // state也是从component继承过来的东西，用于储存属性
+
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -29,6 +37,9 @@ class Main extends Component {
     const HomePage = () => {
       return(
           <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           />
       );
     }
@@ -42,7 +53,9 @@ class Main extends Component {
         {/* <Route path='/home' component={HomePage} /> */}
         <Switch>
               <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />              
+              
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />  
+              <Route exact path='/contactus' component={Contact} />            
               <Redirect to="/home" />
           </Switch>
 
